d90dbd77258ee49a35bc4e7c45d370940d07c974
Kuniaki Shimizu <kun432.8d1w@gmail.com>
2017-01-09 22:46:06 +0900

    example fs

--- app.js
+++ app.js
@@ -7,10 +7,16 @@ server.listen(process.env.PORT || 1234, process.env.IP);
 console.log('Server Running!')
 
 function doRequest(req,res){
+  var number = Math.floor(Math.random()*3);
   fs.readFile('./hello.html', 'UTF-8',
     function(err,data){
+      var title = ["ページA", "ページB", "ページC"];
+      var content = ["※これはサンプルで作ったものです。",
+                     "もう一つのコンテンツです。",
+                     "最後に用意したコンテンツですよ"];
+      var data2 = data.replace(/@title@/g, title[number]).replace(/@content@/, content[number]);
       res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
+      res.write(data2);
       res.end();
     });
 }
