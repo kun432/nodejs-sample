d12db597cd113ab71674a7df287c5586ba65d44f
Kuniaki Shimizu <kun432.8d1w@gmail.com>
2017-01-10 01:47:22 +0900

    add ejs

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 var http = require('http');
 var fs = require('fs');
+var ejs = require('ejs');
 
+var hello = fs.readFileSync('./hello.ejs', 'utf8');
 var server = http.createServer();
 server.on('request',doRequest);
 server.listen(process.env.PORT || 1234, process.env.IP);
 console.log('Server Running!')
 
 function doRequest(req,res){
-  var number = Math.floor(Math.random()*3);
-  fs.readFile('./hello.html', 'UTF-8',
-    function(err,data){
-      var title = ["ページA", "ページB", "ページC"];
-      var content = ["※これはサンプルで作ったものです。",
-                     "もう一つのコンテンツです。",
-                     "最後に用意したコンテンツですよ"];
-      var data2 = data.replace(/@title@/g, title[number]).replace(/@content@/, content[number]);
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data2);
-      res.end();
+    var hello2 = ejs.render(hello, {
+      title:"タイトルです",
+      content:"これはサンプルで作成したテンプレートです",
     });
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(hello2);
+    res.end();
 }
