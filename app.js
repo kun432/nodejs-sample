44e00f641fa10b2d74b580ad95692443be87dac0
Kuniaki Shimizu <kun432.8d1w@gmail.com>
2017-01-09 22:38:30 +0900

    add fs

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer();
 server.on('request',doRequest);
@@ -6,7 +7,10 @@ server.listen(process.env.PORT || 1234, process.env.IP);
 console.log('Server Running!')
 
 function doRequest(req,res){
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello World\n');
-  res.end();
+  fs.readFile('./hello.html', 'UTF-8',
+    function(err,data){
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
 }
